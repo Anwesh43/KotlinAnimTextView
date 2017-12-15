d7460ab44ff85247351120ec42506b8684c8e89d
Anwesh43 <anweshthecool0@gmail.com>
2017-12-15 23:17:39 +0530

    adding TextContainerButton

--- kotlinanimtextview/src/main/java/ui/anwesome/com/kotlinanimtextview/AnimTextView.kt
+++ kotlinanimtextview/src/main/java/ui/anwesome/com/kotlinanimtextview/AnimTextView.kt
@@ -2,7 +2,9 @@ package ui.anwesome.com.kotlinanimtextview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -19,5 +21,23 @@ class AnimTextView(ctx:Context):View(ctx) {
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         return true
+    }
+    data class TextContainerButton(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(45f*scale)
+            paint.strokeWidth = r/8
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.color = Color.parseColor("#1565C0")
+            canvas.drawCircle(0f,0f,r,paint)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.rotate(90f*i)
+                canvas.drawLine(0f,-2*r/3,0f,2*r/3,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
     }
 }
