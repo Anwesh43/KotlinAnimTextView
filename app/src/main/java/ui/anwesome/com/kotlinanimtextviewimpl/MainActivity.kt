b58d3324ca47191c0dddfffbec0d4546a1a45d37
Anwesh43 <anweshthecool0@gmail.com>
2017-12-16 00:19:10 +0530

    adding OnOpenCloseListener in AnimTextView

--- app/src/main/java/ui/anwesome/com/kotlinanimtextviewimpl/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinanimtextviewimpl/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinanimtextviewimpl
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.kotlinanimtextview.AnimTextView
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         AnimTextView.addText("Stag not")
         AnimTextView.addText("worlo world")
         AnimTextView.addText("couple allowed")
+        AnimTextView.addOnOpenCloseListener({Toast.makeText(this,"opened",Toast.LENGTH_SHORT).show()},{Toast.makeText(this,"closed",Toast.LENGTH_SHORT).show()})
         AnimTextView.show(this)
     }
 }
