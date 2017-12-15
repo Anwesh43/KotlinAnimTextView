21f91ef3b386fc25a2f7cde5e01e778456498419
Anwesh43 <anweshthecool0@gmail.com>
2017-12-15 23:58:26 +0530

    creating AnimTextView

--- app/src/main/java/ui/anwesome/com/kotlinanimtextviewimpl/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinanimtextviewimpl/MainActivity.kt
@@ -2,11 +2,21 @@ package ui.anwesome.com.kotlinanimtextviewimpl
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.kotlinanimtextview.AnimTextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AnimTextView.create(this)
+        AnimTextView.addText("hello")
+        AnimTextView.addText("world")
+        AnimTextView.addText("worlo world")
+        AnimTextView.addText("stag not")
+        AnimTextView.addText("Stag not")
+        AnimTextView.addText("worlo world")
+        AnimTextView.addText("couple allowed")
+        AnimTextView.show(this)
     }
 }
